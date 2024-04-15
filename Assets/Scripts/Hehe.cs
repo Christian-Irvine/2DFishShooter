e45cbb622e89a59a2cce92e3d47461df65acdbfe
Christian Irvine <christian.irvine04@gmail.com>
2024-04-15 19:22:38 +1200

    Added Hehe class and Haha method

---
+++ Assets/Scripts/Hehe.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Hehe
+{
+    public static void Haha()
+    {
+        Debug.Log("HeheHaha");
+    }
+}
